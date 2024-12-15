c6106a91a96afca3d7f6ae257a267a8c2b7d5818
stillhue <82818688+StillHue@users.noreply.github.com>
2024-12-15 16:12:53 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,18 +18,32 @@ def login_ao_sistema(usuario, senha):
         return None
 
 def buscar_chamados(session, filtro):
-    url = 'https://portalllk.lanlink.com.br/assystweb/chamados.do'
-    response = session.get(url)
+    url_filtro = 'https://portalllk.lanlink.com.br/assystweb/application.do#eventsearch%2FEventSearchDelegatingDispatchAction.do?dispatch=monitorInitNoResults'
+    response = session.get(url_filtro)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         dados = []
-        chamados = soup.find_all('div', class_='chamado-row')
+
+        if filtro == "TJRN - 1N CHAMADOS":
+            chamados = soup.find_all('div', class_='chamado-row')  # Ajuste conforme o filtro
+        elif filtro == "TJRN - 2N CHAMADOS":
+            chamados = soup.find_all('div', class_='chamado-row')  # Ajuste conforme o filtro
+        else:
+            chamados = []  # Ajuste caso o filtro não seja válido
+
         for chamado in chamados:
-            numero = chamado.find('span', class_='numero').text.strip()
-            descricao = chamado.find('span', class_='descricao').text.strip()
-            status = chamado.find('span', class_='status').text.strip()
-            data_abertura = chamado.find('span', class_='data-abertura').text.strip()
-            dados.append([numero, descricao, status, data_abertura])
+            numero = chamado.find('span', class_='numero')
+            descricao = chamado.find('span', class_='descricao')
+            status = chamado.find('span', class_='status')
+            data_abertura = chamado.find('span', class_='data-abertura')
+
+            if numero and descricao and status and data_abertura:
+                dados.append([
+                    numero.text.strip(),
+                    descricao.text.strip(),
+                    status.text.strip(),
+                    data_abertura.text.strip()
+                ])
         return pd.DataFrame(dados, columns=['Número', 'Descrição', 'Status', 'Data de Abertura'])
     else:
         st.error(f"Falha ao buscar dados. Código de status: {response.status_code}")
